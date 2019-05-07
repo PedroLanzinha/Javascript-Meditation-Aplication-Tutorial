b7d0e7d45b6613d398b02ba9cde6d6c68fb1d318
unknown <theitalianpeter@hotmail.com>
2019-05-07 23:34:45 +0100

    error commit

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const app = () =>{
-    const song = document.querySelector('.song');
-    const play = document.querySelector('.play');
-    const outline = document.querySelector('.moving-outline circle');
-    const video = document.querySelector('.vid-container video');
+    const song = document.querySelector(".song");
+    const play = document.querySelector(".play");
+    const outline = document.querySelector(".moving-outline circle");
+    const video = document.querySelector(".vid-container video");
 
     //Sounds
-    const sounds = document.querySelectorAll('.sound-picker button');
+    const sounds = document.querySelectorAll(".sound-picker button");
     //Time Displayer
-    const timeDisplay = document.querySelector('.timeDisplay')
+    const timeDisplay = document.querySelector(".time-Display");
     //get lenght of circular outline
-    const outlineLenght = outline.getTotalLenght();
-    console.log(outlineLenght);
-}
+    const outlineLength = outline.getTotalLenght();
+    //console.log(outlineLenght);
+    //Duration
+    let fakeDuration = 600;
+
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = 200;
+
+};
 
 app();
